286eef6938b90a6979c2163e52edc7b9868a458c
KeiserDallas3205 <62128647+KeiserDallas3205@users.noreply.github.com>
2023-04-06 19:37:12 -0500

    Changed test scene

---
+++ VR Simulator/Assets/Scripts/sceneChange.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    
+	public void changeScene(int sceneID) {}
+	
+	public void quit(){}
+
+}
