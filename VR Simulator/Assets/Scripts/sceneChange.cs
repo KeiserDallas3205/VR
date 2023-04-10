577385a56f807c964a7436f50fe02130c2233811
KeiserDallas3205 <62128647+KeiserDallas3205@users.noreply.github.com>
2023-04-10 16:22:20 -0500

    Added start menu prefab and scene changer script

--- VR Simulator/Assets/Scripts/sceneChange.cs
+++ VR Simulator/Assets/Scripts/sceneChange.cs
@@ -1,12 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class Test : MonoBehaviour
+public class sceneChange : MonoBehaviour
 {
     
-	public void changeScene(int sceneID) {}
+	public void changeScene(int sceneID) {
+		SceneManager.LoadScene(sceneID);
+		print("Changed scene");
+	}
 	
-	public void quit(){}
+	public void quit(){
+		print("bye");
+		 Application.Quit();
+	}
+	
+	public void load(){
+		print("Load last save");
+	}
 
 }
